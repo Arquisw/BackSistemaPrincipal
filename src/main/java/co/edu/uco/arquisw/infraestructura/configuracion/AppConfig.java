104f41fd72905e0fbfb96ce2f1b7ed7937547a91
ESTEBAN\Esteban <estebancoloradogonzalez@gmail.com>
2023-04-14 19:48:53 -0500

    Se formatea el codigo de AppConfig

--- src/main/java/co/edu/uco/arquisw/infraestructura/configuracion/AppConfig.java
+++ src/main/java/co/edu/uco/arquisw/infraestructura/configuracion/AppConfig.java
@@ -3,6 +3,7 @@ package co.edu.uco.arquisw.infraestructura.configuracion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
 @Configuration
 public class AppConfig {
     @Bean
