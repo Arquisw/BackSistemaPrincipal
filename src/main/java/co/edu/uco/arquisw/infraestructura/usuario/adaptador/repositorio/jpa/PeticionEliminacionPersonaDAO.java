32a8142d22b4f423adbe1b4e06ee69efa694d9fd
juanvalenciaceiba <107130614+juanvalenciaceiba@users.noreply.github.com>
2023-08-10 11:48:23 -0500

    Se organiza flojo de eliminacion de usuario

--- src/main/java/co/edu/uco/arquisw/infraestructura/usuario/adaptador/repositorio/jpa/PeticionEliminacionPersonaDAO.java
+++ src/main/java/co/edu/uco/arquisw/infraestructura/usuario/adaptador/repositorio/jpa/PeticionEliminacionPersonaDAO.java
@@ -1,9 +1,9 @@
 package co.edu.uco.arquisw.infraestructura.usuario.adaptador.repositorio.jpa;
 
-import co.edu.uco.arquisw.infraestructura.usuario.adaptador.entidad.HojaDeVidaPersonaEntidad;
+
 import co.edu.uco.arquisw.infraestructura.usuario.adaptador.entidad.PeticionEliminacionPersonaEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PeticionEliminacionPersonaDAO extends JpaRepository<PeticionEliminacionPersonaEntidad, Long> {
-    HojaDeVidaPersonaEntidad findByUsuario(Long usuario);
+    PeticionEliminacionPersonaEntidad findByUsuario(Long usuario);
 }
