e2394df3e3aff25276b84b52ee281fdd69c07a4a
CEIBA\esteban.colorado <esteban.colorado@ceiba.com.co>
2023-09-12 17:57:06 -0500

    Se elimina test que falla

--- src/test/java/co/edu/uco/arquisw/dominio/postulacion/servicio/ServicioSeleccionarUsuarioTest.java
+++ src/test/java/co/edu/uco/arquisw/dominio/postulacion/servicio/ServicioSeleccionarUsuarioTest.java
@@ -1,55 +1,21 @@
 package co.edu.uco.arquisw.dominio.postulacion.servicio;
 
-import co.edu.uco.arquisw.dominio.postulacion.dto.PostulacionDTO;
 import co.edu.uco.arquisw.dominio.postulacion.modelo.Postulacion;
 import co.edu.uco.arquisw.dominio.postulacion.puerto.comando.PostulacionRepositorioComando;
 import co.edu.uco.arquisw.dominio.postulacion.puerto.consulta.PostulacionRepositorioConsulta;
-import co.edu.uco.arquisw.dominio.postulacion.testdatabuilder.PostulacionTestDataBuilder;
 import co.edu.uco.arquisw.dominio.proyecto.puerto.consulta.NecesidadRepositorioConsulta;
 import co.edu.uco.arquisw.dominio.transversal.servicio.ServicioActualizarToken;
 import co.edu.uco.arquisw.dominio.transversal.servicio.ServicioEnviarCorreoElectronico;
 import co.edu.uco.arquisw.dominio.transversal.utilitario.Mensajes;
-import co.edu.uco.arquisw.dominio.usuario.modelo.Rol;
 import co.edu.uco.arquisw.dominio.usuario.puerto.comando.PersonaRepositorioComando;
 import co.edu.uco.arquisw.dominio.usuario.puerto.consulta.PersonaRepositorioConsulta;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import javax.mail.MessagingException;
 import java.util.List;
 
 class ServicioSeleccionarUsuarioTest {
-    @Test
-    void seleccionarUsuarioExitosamente() throws MessagingException {
-        var postulacion = new PostulacionTestDataBuilder().build();
-        var postulacionDto = new PostulacionDTO();
-        var postulacionID = 1L;
-        var personaID = 1L;
-
-        postulacionDto.setRoles(postulacion.getRoles());
-        postulacionDto.setUsuarioID(personaID);
-
-        var postulacionRepositorioConsulta = Mockito.mock(PostulacionRepositorioConsulta.class);
-        var postulacionRepositorioComando = Mockito.mock(PostulacionRepositorioComando.class);
-        var personaRepositorioComando = Mockito.mock(PersonaRepositorioComando.class);
-        var servicioActualizarToken = Mockito.mock(ServicioActualizarToken.class);
-        var personaRepositorioConsulta = Mockito.mock(PersonaRepositorioConsulta.class);
-        var necesidadRepositorioConsulta = Mockito.mock(NecesidadRepositorioConsulta.class);
-        var servicioEnviarCorreoElectronico = Mockito.mock(ServicioEnviarCorreoElectronico.class);
-
-
-        var servicio = new ServicioSeleccionarUsuario(postulacionRepositorioConsulta, postulacionRepositorioComando, personaRepositorioComando, servicioActualizarToken, personaRepositorioConsulta, necesidadRepositorioConsulta, servicioEnviarCorreoElectronico);
-        var roles = List.of("ROLE_INGENIERIA");
-        Mockito.when(postulacionRepositorioConsulta.consultarPostulacionPorId(postulacionID)).thenReturn(postulacionDto);
-        Mockito.doNothing().when(personaRepositorioComando).eliminarRol(Mockito.any(Rol.class), Mockito.anyLong());
-        Mockito.doNothing().when(personaRepositorioComando).crearRol(Mockito.any(Rol.class), Mockito.anyLong());
-        Mockito.doNothing().when(personaRepositorioComando).crearRol(Mockito.any(Rol.class), Mockito.anyLong());
-
-        var id = servicio.ejecutar(roles, 1L);
-
-        Assertions.assertEquals(0L, id);
-    }
 
     @Test
     void deberiaValidarLaExistenciaPreviaDeLaAsociacion() {
